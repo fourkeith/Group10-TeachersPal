4b932335c905d813b255943096e81304be794096
fourkeith <nemimimonkey@gmail.com>
2017-12-01 01:30:21 -0600

    Started Voice Changer code. It is coming up but when you hit record it crashes.

--- app/src/main/java/com/example/fitfourdesktop/group10_teacherspal/VoiceChanger.java
+++ app/src/main/java/com/example/fitfourdesktop/group10_teacherspal/VoiceChanger.java
@@ -1,7 +1,20 @@
 package com.example.fitfourdesktop.group10_teacherspal;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
 
 /**
  * @author Tyler Wilson
@@ -10,13 +23,34 @@ import android.os.Bundle;
  */
 public class VoiceChanger extends AppCompatActivity {
 
+    audioRecord recorder;
+    boolean start = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voice_changer);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        recorder = new audioRecord();
+    }
+
+    public void onClickRecord(View view) {
+        recorder.onRecord(start);
+        if (start)
+            start = false;
+        else
+            start = true;
+    }
+
+    public void onClickPlay(View view) {
+        recorder.onPlay(start);
+        if (start)
+            start = false;
+        else
+            start = true;
     }
 
     public void onClickReturn() {}
+
 }
---
+++ app/src/main/java/com/example/fitfourdesktop/group10_teacherspal/audioRecord.java
@@ -0,0 +1,95 @@
+package com.example.fitfourdesktop.group10_teacherspal;
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.IOException;
+
+/**
+ * Created by Fitfour Desktop on 11/29/2017.
+ */
+
+
+public class audioRecord extends AppCompatActivity{
+
+    private static String mFileName = null;
+
+    private MediaRecorder mRecorder = null;
+
+    private MediaPlayer   mPlayer = null;
+
+    audioRecord() {
+        mRecorder = new MediaRecorder();
+        mPlayer = new MediaPlayer();
+        mFileName = "myRecording";
+    }
+
+
+    public void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    public void onPlay(boolean start) {
+        if (start) {
+            startPlaying();
+        } else {
+            stopPlaying();
+        }
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+        }
+    }
+
+    private void stopPlaying() {
+        mPlayer.release();
+        mPlayer = null;
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+}
